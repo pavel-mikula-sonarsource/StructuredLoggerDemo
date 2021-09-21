5c735945320f5e0ea0367e233aa37986fb3c049b
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-09-21 10:37:17 +0200

    Read log

--- StructuredLoggerDemo/Program.cs
+++ StructuredLoggerDemo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Build.Logging.StructuredLogger;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace StructuredLoggerDemo
 {
@@ -6,7 +9,29 @@ namespace StructuredLoggerDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "build.binlog");
+            var root = BinaryLog.ReadBuild(path);
+            // Find interesting node
+            //root.VisitAllChildren<TreeNode>(x =>
+            //{
+            //    if (x.ToString().Contains(".Rules."))
+            //    {
+            //        Console.WriteLine(x.Parent.GetType().Name + " / " + x.GetType().Name + ": " + x.ToString());
+            //    }
+            //    if (x.ToString().Contains("Total analyzer execution time"))
+            //    {
+            //        Console.WriteLine(x.Parent.GetType().Name + " / " + x.GetType().Name + ": " + x.ToString());
+            //    }
+            //});
+            // Read/Parse
+            root.VisitAllChildren<CscTask>(x =>
+            {
+                foreach(var message in x.Children.OfType<Message>())
+                {
+                    Console.WriteLine(message.Text); // All message, including ones that we're not interested in
+                }
+            });
+            Console.ReadKey();
         }
     }
 }
